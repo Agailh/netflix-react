6f22da0710336fdda668efcd300a0e05541972fe
Agailh <agailham86@gmail.com>
2023-03-31 14:29:22 +0700

    commit

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
 import Main from '../Components/Main'
+import Row from '../Components/Row'
+import requests from '../Request'
 
 const Home = () => {
   return (
     <div>
         <Main />
+        <Row title='UpComing' fetchURL={requests.requestUpcoming} />
+        <Row title='Popular' fetchURL={requests.requestPopular} />
+        <Row title='Trending' fetchURL={requests.requestTrending} />
+        <Row title='Horror' fetchURL={requests.requestHorror} />
+        <Row title='TopRated' fetchURL={requests.requestTopRated} />
     </div>
   )
 }
--- src/Request.js
+++ src/Request.js
@@ -1,11 +1,11 @@
 const key = '66c8e19ae0ec97513a5271fe9375c38c'
 
 const requests = {
-    requestPopular: `https://api.themoviedb.org/3/movie/popular?api_key=${key}&language=en-USpage=1`,
-    requestTopRated: `https://api.themoviedb.org/3/movie/top_rated?api_key=${key}&language=en-USpage=1`,
-    requestTrending: `https://api.themoviedb.org/3/movie/popular?api_key=${key}&language=en-USpage=2`,
-    requestHorror: `https://api.themoviedb.org/3/movie/movie?api_key=${key}&language=en-US&query=horror&page=1&include_adult=false`,
-    requestUpcoming: `https://api.themoviedb.org/3/movie/popular?api_key=${key}&language=en-USpage=1`,
+    requestPopular: `https://api.themoviedb.org/3/movie/popular?api_key=${key}&language=en-USpage=1&include_adult=false`,
+    requestTopRated: `https://api.themoviedb.org/3/movie/top_rated?api_key=${key}&language=en-USpage=1&include_adult=false`,
+    requestTrending: `https://api.themoviedb.org/3/movie/popular?api_key=${key}&language=en-USpage=2&include_adult=false`,
+    requestHorror: `https://api.themoviedb.org/3/discover/movie?api_key=${key}&language=en-US&with_genres=27&page=1&include_adult=false`,
+    requestUpcoming: `https://api.themoviedb.org/3/movie/popular?api_key=${key}&language=en-USpage=1&include_adult=false`,
     
     
 };
