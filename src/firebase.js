35172af6ac6918beba411c68ae089a035a60e04a
Agailh <agailham86@gmail.com>
2023-04-03 11:07:19 +0700

    hide api key env

--- src/firebase.js
+++ src/firebase.js
@@ -7,14 +7,17 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBjC_EozNfOcgkaxDsEsG5nyzrQ9OXLqXM",
-  authDomain: "netflix-react-d682b.firebaseapp.com",
-  projectId: "netflix-react-d682b",
-  storageBucket: "netflix-react-d682b.appspot.com",
-  messagingSenderId: "925801643051",
-  appId: "1:925801643051:web:a33a5df40a807d55b2fe6e",
-  measurementId: "G-2V2P5NCK7N"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain:process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId:process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGIN_SENDER,
+  appId:process.env.REACT_APP_APP_ID,
+  
 };
+
+
+
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
