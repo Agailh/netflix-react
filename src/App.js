a7134ab186cf64dafed97c89651351eb4cb9e77a
Agailh <agailham86@gmail.com>
2023-04-03 12:21:00 +0700

    account page

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import { Route, Routes } from "react-router-dom";
 import Navbar from "./Components/Navbar";
 import Home from "./Pages/Home";
+import { AuthContextProvider } from "./context/AuthContext";
 
 
 function App() {
   return (
     <div >
+      <AuthContextProvider>
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
-
       </Routes>
+      </AuthContextProvider>
     </div>
   );
 }
