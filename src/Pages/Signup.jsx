7bb96b30c3a89fe137cb5c0e68880bf11dca0998
Agailh <agailham86@gmail.com>
2023-04-03 14:25:12 +0700

    sign in up page

--- src/Pages/Signup.jsx
+++ src/Pages/Signup.jsx
@@ -1,9 +1,34 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Signup = () => {
   return (
-    <div>Signup</div>
-  )
+    <>
+    <div className='w-full h-screen '>
+        <img className='hidden sm:block absolute w-full h-ful object-cover' src='https://www.logitheque.com/en/wp-content/uploads/sites/6/2019/07/netflix-image.jpg' alt='/' />
+            <div className=' bg-black/60 fixed top-0 left-0 w-full h-screen  '></div>
+            <div className='fixed w-full px-4 py-24 z-50 '>
+                <div className='max-w-[450px] h-[600px] mx-auto bg-black/75 text-white'>
+                    <div className='max-w-[320px] mx-auto py-16'>
+                        <h1 className='text-3xl font-bold'>Sign Up</h1>
+                        <form className='w-full flex flex-col py-4'>
+                            <input className='p-3 my-2 bg-gray-500 rounded' type="email" placeholder='Email' autoComplete='email' />
+                            <input className='p-3 my-2 bg-gray-500 rounded' type="password" placeholder='Password' autoComplete='current-password'/>
+                            <button className='bg-red-600 py-3 my-6 rounded font-bold'>Sign Up</button>
+                            <div className='flex justify-between items-center text-sm text-gray-600'>
+                                <p><input className='mr-2' type="checkbox" />Remember me</p>
+                                <p>Need Help?</p>
+                            </div>
+                                <p className='py-8 text-gray-600'>Already Subscribed to Netflix? <span className='text-white'><Link to='/login'>Sign In</Link></span></p>
+                        </form>
+                    </div>
+
+                </div>
+            </div>
+
+    </div>
+    </>
+)
 }
 
 export default Signup
