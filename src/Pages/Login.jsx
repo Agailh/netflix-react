2e7e9b81c84072c9049b4e401f77566ec244c1a0
Agailh <agailham86@gmail.com>
2023-04-03 16:12:47 +0700

    kurang login

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,7 +1,22 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React, { useState } from 'react'
+import { Link ,useNavigate } from 'react-router-dom'
+import { UserAuth } from '../context/AuthContext'
 
 const Login = () => {
+    const [email, setEmail]= useStateate ('')
+    const [password, setPassword] = useState('')
+    const {user, logIn } = UserAuth ()
+    const navigate = useNavigate ()
+
+    const handleSumbit = async (e) => {
+        e.preventDefault()
+        try {
+            await logIn(email, password)
+            navigate('/')
+        }catch (error) {
+            console.log(error)
+        }
+    }
   return (
     <>
     <div className='w-full h-screen '>
@@ -11,7 +26,7 @@ const Login = () => {
                 <div className='max-w-[450px] h-[600px] mx-auto bg-black/75 text-white'>
                     <div className='max-w-[320px] mx-auto py-16'>
                         <h1 className='text-3xl font-bold'>Sign In</h1>
-                        <form className='w-full flex flex-col py-4'>
+                        <form onSubmit={handleSumbit } className='w-full flex flex-col py-4'>
                             <input className='p-3 my-2 bg-gray-500 rounded' type="email" placeholder='Email' autoComplete='email' />
                             <input className='p-3 my-2 bg-gray-500 rounded' type="password" placeholder='Password' autoComplete='current-password'/>
                             <button className='bg-red-600 py-3 my-6 rounded font-bold'>Sign In</button>
