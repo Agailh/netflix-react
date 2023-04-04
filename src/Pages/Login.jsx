e0fb893c1a994397ea77e37315da7cdd1af7628b
Agailh <agailham86@gmail.com>
2023-04-04 11:57:53 +0700

    error massage still error

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -5,16 +5,19 @@ import { UserAuth } from '../context/AuthContext'
 const Login = () => {
     const [email, setEmail]= useState ('')
     const [password, setPassword] = useState('')
-    const {user, logIn } = UserAuth ()
+    const [error, setError] = useState ('')
+     const {user, logIn } = UserAuth ()
     const navigate = useNavigate ()
 
     const handleSumbit = async (e) => {
         e.preventDefault()
+        setError('')
         try {
             await logIn(email, password)
             navigate('/')
         }catch (error) {
             console.log(error)
+            setError(error.massage)
         }
     }
   return (
@@ -26,9 +29,10 @@ const Login = () => {
                 <div className='max-w-[450px] h-[600px] mx-auto bg-black/75 text-white'>
                     <div className='max-w-[320px] mx-auto py-16'>
                         <h1 className='text-3xl font-bold'>Sign In</h1>
+                        {error ? <p className='p-3 bg-red-400 my-2'>{error}</p> :  null}
                         <form onSubmit={handleSumbit } className='w-full flex flex-col py-4'>
-                            <input className='p-3 my-2 bg-gray-500 rounded' type="email" placeholder='Email' autoComplete='email' />
-                            <input className='p-3 my-2 bg-gray-500 rounded' type="password" placeholder='Password' autoComplete='current-password'/>
+                            <input onChange={(e) => setEmail (e.target.value)} className='p-3 my-2 bg-gray-500 rounded' type="email" placeholder='Email' autoComplete='email' />
+                            <input onChange={(e) => setPassword (e.target.value)} className='p-3 my-2 bg-gray-500 rounded' type="password" placeholder='Password' autoComplete='current-password'/>
                             <button className='bg-red-600 py-3 my-6 rounded font-bold'>Sign In</button>
                             <div className='flex justify-between items-center text-sm text-gray-600'>
                                 <p><input className='mr-2' type="checkbox" />Remember me</p>
